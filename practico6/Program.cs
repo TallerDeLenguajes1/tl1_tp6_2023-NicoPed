cfafffeeb404f191622231c5c8c04ae50429e580
NicoPed <nikopedraza10@gmail.com>
2023-05-18 23:36:43 -0300

    CalculadoraV2 commit inicial

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,43 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*Console.WriteLine("Hello, World!");
-
-int a;
-int b;
-
-a = 10;
-b = a;
-Console.WriteLine("Valor de a: " + a);
-Console.WriteLine("Valor de b: " + b);
+﻿/*Ejercicio 2. Ingrese al branch CalculadoraV1 y construya un programa que sea una
+calculadora que permita al usuario realizar las 4 operaciones básicas (Sumar, Restar,
+Multiplicar y Dividir) a partir de un menú para seleccionar la opción a elegir y que luego
+pida dos números y que devuelva el resultado de la operación seleccionada. Además
+una vez que termine de realizar la operación le pregunte si desea realizar otro cálculo.
 */
-//EJERCICIO 1
-/*Construir un programa que permita invertir un número. Verifique que el
-texto ingresado es de hecho un número y, en caso afirmativo, realice la inversión del
-número sólo si éste es mayor a 0.
-*/
-int numero = 0;
-bool esNumero ;
-string?  numString ="";
-string numInvertido ="";
+int opcion;
+string? auxiliarLeer;
+string? auxiliarLeer2;
 
+float num1= 0, num2 = 0;
 do
 {
-    Console.WriteLine("\nIngresa un número valido");
-    numString = Console.ReadLine();
-    esNumero = int.TryParse(numString, out numero);
-    if (!esNumero)
-    {
-        Console.WriteLine(numString + "\nNO es un número válido");
+    
+    Console.WriteLine("========== MENÚ ==========");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("5. Valor Absoluto");
+    Console.WriteLine("6. Cuadrado");
+    Console.WriteLine("7. Raiz Cuadrada");
+    Console.WriteLine("8. Seno");
+    Console.WriteLine("9. Coseno");
+    Console.WriteLine("10. Parte entera de un real");
+    Console.WriteLine("0. Salir");
+    Console.WriteLine("==========================");
+    Console.WriteLine("Ingrese una opcion: ");
+    auxiliarLeer = Console.ReadLine();
+    if (int.TryParse(auxiliarLeer ,out opcion)){
+        
+        if (opcion > 5 || opcion < 0)
+        {
+            Console.WriteLine("Ingrese una opcion correcta");
+        }
+        else if(opcion != 0)
+        { 
+            Console.WriteLine("Ingrese el primer numero: ");
+            auxiliarLeer = Console.ReadLine();
+            Console.WriteLine("Ingrese el segundo numero: ");
+            auxiliarLeer2 = Console.ReadLine();
+            if (float.TryParse(auxiliarLeer ,out num1) && float.TryParse(auxiliarLeer2 ,out num2))
+            { 
+                Console.WriteLine("==========================");
+                switch (opcion)
+                {
+                    case 0: 
+                            Console.WriteLine("Gracias por usarnos :)");
+                        break;
+                    case 1: 
+                            Console.WriteLine(num1+" + "+num2+" = "+ (num1 + num2));
+                        break;
+                    case 2: 
+                            Console.WriteLine(num1+" - "+num2+" = "+ (num1 - num2));
+                        break;
+                    case 3: 
+                            Console.WriteLine(num1+" * "+num2+" = "+ (num1 * num2));
+                        break;
+                    case 4: 
+                            Console.WriteLine(num1+" / "+num2+" = "+ (num1 / num2));
+                        break;
+                }
+                Console.WriteLine("==========================");
+            }
+        }
+        
     }
     else
     {
-        if (numero > 0)
-        {    
-            while (numero >= 1)
-            {
-                numInvertido = numInvertido + (numero % 10);
-                numero = numero/10;
-            }
-            Console.WriteLine(numInvertido);
-        }
+        opcion = 99999;
     }
-} while (!esNumero);
+} while (opcion != 0);

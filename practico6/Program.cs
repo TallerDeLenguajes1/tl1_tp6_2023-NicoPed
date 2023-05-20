917ec391bcad3c858dbd53f1cdcce3cebe944d4e
NicoPed <nikopedraza10@gmail.com>
2023-05-19 22:31:35 -0300

    Realizado mayoría del Ejecicio 4, falta ultimo inciso

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -13,7 +13,6 @@ Console.WriteLine("Valor de b: " + b);
 /*Construir un programa que permita invertir un número. Verifique que el
 texto ingresado es de hecho un número y, en caso afirmativo, realice la inversión del
 número sólo si éste es mayor a 0.
-*/
 int numero = 0;
 bool esNumero ;
 string?  numString ="";
@@ -41,3 +40,109 @@ do
         }
     }
 } while (!esNumero);
+*/ 
+//================= EJERCICIO 4 =====================
+// Dada una cadena (un string) de texto ingresada por el usuario, realice las siguientes
+// tarea:
+
+string? cadena, cadena2;
+Console.Write("Ingrese un texto: ");
+cadena = Console.ReadLine();
+Console.WriteLine("========== LONGITUD ===========");
+// ● Obtener la longitud de la cadena y muestre por pantalla.
+if (cadena != null)
+{
+int longitud = cadena.Length;
+Console.WriteLine($"La longitud de la cadena es {longitud}");
+}
+
+// ● A partir de una segunda cadena ingresada por el usuario, concatene ambas
+// cadenas distintas.
+Console.WriteLine("========== CONCATENAR ===========");
+Console.Write("Ingrese otro texto: ");
+cadena2 = Console.ReadLine();
+string concatenada = cadena +" "+cadena2;
+Console.WriteLine("Concatenadas: "+ concatenada);
+
+// ● Extraer una subcadena de la cadena ingresada.
+Console.WriteLine("========== CONCATENAR ===========");
+string? subcadena;
+if (cadena != null && cadena.Length > 5)
+{
+subcadena = cadena.Substring(0,4);
+Console.WriteLine("SubCadena: "+ subcadena);
+}
+
+// ● Utilizando la calculadora creada anteriormente realizar las operaciones de dos
+// números y mostrar por pantalla y mostrar en texto el resultado. Por ejemplo para
+// la suma sería:
+// “la suma de “ num1 “ y de” num2 “ es igual a: ” resultado.
+// Donde num1, num2 y resultados son los sumandos y el resultado de la operación
+// respectivamente.
+// Nota: Busque el comportamiento del Método ToString();
+// ● Recorrer la cadena de texto con un ciclo Foreach e ir mostrando elemento por
+// elemento en pantalla
+Console.WriteLine("========== FOREACH ===========");
+int num1 = 7;
+int num2 = 6;
+int resultado = num1 +num2;
+
+//string res = "La suma de "+num1+" y de "+num2+" es igual a : "+resultado+"";
+string res = $"La suma de {num1} y de {num2} es igual a : {resultado}";
+
+foreach (char caracter in res)
+{
+    Console.WriteLine(caracter);
+}
+// ● Buscar la ocurrencia de una palabra determinada en la cadena ingresada
+Console.WriteLine("========== OCURRENCIA ===========");
+int cantOcurrencias = 0;
+Console.Write("Ingrese un texto: ");
+cadena = Console.ReadLine();
+string? palabra;
+int indice;
+Console.Write("Ingrese la palabra a buscar: ");
+palabra = Console.ReadLine();
+if (cadena != null && palabra != null)
+{
+    indice = cadena.IndexOf(palabra);
+    while (indice != -1)
+    {
+        cantOcurrencias ++;
+        cadena = cadena.Substring(indice + 1);
+        indice = cadena.IndexOf(palabra);
+    }    
+}
+Console.WriteLine($"La cantidad de ocurrencias de '{palabra}' en el texto son: {cantOcurrencias}");
+// ● Convierta la cadena a mayúsculas y luego a minúsculas.
+Console.WriteLine("========== MAYUS Y MINUS ===========");
+Console.Write("Ingrese un texto: ");
+cadena = Console.ReadLine();
+if (cadena != null)
+{
+cadena = cadena.ToUpper();
+Console.WriteLine(cadena);
+cadena = cadena.ToLower();
+Console.WriteLine(cadena);
+}
+// ● Ingrese una cadena separada por caracteres que usted determine y muestre por
+// pantalla los resultados (Revisar el comportamiento de split())
+Console.WriteLine("========== SEPARAR ===========");
+string? aSeparar ="Te,Quiero,Dana";
+string []separado = aSeparar.Split(',');
+foreach (string minicadena in separado)
+{
+    Console.WriteLine(minicadena);
+    
+}
+foreach (var minicadena in separado.Select((valor,indice) => new {Valor = valor, Indice = indice} ))
+{
+    Console.WriteLine($"Índice: {minicadena.Indice}, Valor: {minicadena.Valor}");
+
+}
+
+// ● Siguiendo con el ejemplo de la calculadora (ejercicio 2) ingrese una ecuación
+// simple como cadena de caracteres y que el sistema lo resuelva. Por ej. ingrese
+// por pantalla “582+2” y que le devuelva la suma de 582 con 2
+Console.WriteLine("========== ESCRIBIR ECUACION ===========");
+string ecuacion;
